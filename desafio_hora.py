f0e92e75f451a869e5750be4588c8d1eeb27e6d2
Caio Marinho <108732958+Caio-Marinho@users.noreply.github.com>
2024-09-05 11:10:23 -0300

    Add files via upload

--- desafio_hora.py
+++ desafio_hora.py
@@ -5,19 +5,12 @@ min2 = int(input("minuto2: "))
 hora_excedente = (min1 + min2) // 60
 minuto_final = (min1 + min2) % 60
 hora_final = hora1 + hora2 + hora_excedente + 12
-if (hora_final < 0 or hora_final > 12):
-    hora_excedente = (min1 + min2) // 60
-    minuto_final = (min1 + min2) % 60
-    hora_final = hora1 + hora2 + hora_excedente + 12
-    hora_final = hora_final - 12 - 24
+if hora_final < 0 or hora_final > 12:
+    hora_final = hora_final - 36
     if hora_final < 0:
         hora_final *= -1
     if hora_final > 12:
         hora_final -= 12
-    print(f"entrada 1: {hora1}:{min1}")
-    print(f"entrada 2: {hora2}:{min2}")
-    print(f"saida : {hora_final}:{minuto_final}")
-else:
     print(f"entrada 1: {hora1}:{min1}")
     print(f"entrada 2: {hora2}:{min2}")
     print(f"saida : {hora_final}:{minuto_final}")
